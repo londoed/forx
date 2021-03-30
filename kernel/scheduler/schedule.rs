472d61f5ab75ce6f47b6973fe48d77e43b706b81
Eric Londo <londoed@comcast.net>
2021-03-30 12:16:59 -0400

    Initial commit

---
+++ kernel/scheduler/schedule.rs
@@ -0,0 +1,10 @@
+//
+  // FORK: An open and collaborative research operating system kernel.
+  //
+  // Copyright (C) 2021, Eric Londo <londoed@comcast.net>, { kernel/scheduler/schedule.rs }.
+  // This Sorce Code Form is subject to the terms of the Mozilla Public License v 2.0.
+  // If a copy of the MPL was not distributed with this file, you can obtain one at:
+  // https://mozilla.org/MPL/2.0.
+//
+
+
